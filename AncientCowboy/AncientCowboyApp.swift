3359f5be47560bfc617aeea18306a0fef2d08bc5
Riley Usagi <riley.usagi@gmail.com>
2021-08-02 11:59:45 +0300

    Init

---
+++ AncientCowboy/AncientCowboyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AncientCowboyApp.swift
+//  AncientCowboy
+//
+//  Created by Riley Usagi on 02.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct AncientCowboyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
