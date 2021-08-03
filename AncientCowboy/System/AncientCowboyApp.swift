912834e5cc58f6e8f9d0717eeab0583bea039d86
Riley Usagi <riley.usagi@gmail.com>
2021-08-03 12:19:53 +0300

    Prepare registration

--- AncientCowboy/System/AncientCowboyApp.swift
+++ AncientCowboy/System/AncientCowboyApp.swift
@@ -1,15 +1,46 @@
 import SwiftUI
 
+/// Статус регистрации пользователя
+enum RegistrationStatus: String, CaseIterable {
+  case unknown            = "unknown"
+  case notRegistered      = "notRegistered"
+  case alreadyRegistered  = "alreadyRegistered"
+}
+
+/// Точка входа в приложение
 @main struct AncientCowboyApp: App {
   
+  
   // MARK: - Параметры
+  
+  /// Текущий статус регистрации пользователя
+  @State private var registrationStatus: RegistrationStatus = .notRegistered
   
   /// Объект окружения приложения
   let environment: AppEnvironment
   
   var body: some Scene {
     WindowGroup {
-      ContentView(container: environment.container)
+      
+      Group {
+        
+        switch registrationStatus {
+        
+        case .unknown:
+          AnyView(
+            Text("Unknown")
+          )
+          
+        case .notRegistered:
+          AnyView(Text("Registration view"))
+          
+        case .alreadyRegistered:
+          AnyView(ContentView(container: environment.container))
+        }
+      }
+      .onAppear {
+        print("Trying load hero from FireBase")
+      }
     }
   }
   
